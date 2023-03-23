0d491ca96b5974fd2a4b9c67490ff31868a95c36
Larissa Corrẽa <larimscorrea@gmail.com>
2023-03-22 21:27:46 -0300

    mais um passo

--- nodejs-try1/app.js
+++ nodejs-try1/app.js
@@ -15,10 +15,31 @@ app.post('/submit', (req, res) => {
 
 });
 
-app.listen(2612, () => {
-  console.log('Servidor iniciado na porta 2612.');
+app.listen(3000, () => {
+  console.log('Servidor iniciado na porta 3000.');
   
 });
 
 app.listen();
+
+var http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// Outra forma de escrever o de cima com menos código.
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hello World!');
+// }).listen(8080);
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 
